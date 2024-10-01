fe6d7df21594378587a655d46bd98539658b4eaf
Aywenm <162980055+Aywenm@users.noreply.github.com>
2024-10-01 09:18:46 +0200

    Add files via upload

--- st_streamlit.py
+++ st_streamlit.py
@@ -3,7 +3,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+from geopy.geocoders import Nominatim
 
-data = requests.get("https://api.mapbox.com/search/searchbox/v1/suggest?q={search_text}").json()
 
-st.write(data)
+
+
+geolocator = Nominatim(user_agent="https://nominatim.openstreetmap.org/ui/search.html")
+
+text_location_depart = st.text_input("Adress_depart")
+location = geolocator.geocode(text_location_depart)
+text_location_arrivee = st.text_input("Adress_arrivee")
+location1 = geolocator.geocode(text_location_arrivee)
+
+
+print(location.address)
+print((location.latitude, location.longitude))
+print(location.raw)
+data = {"lat": [location.latitude, location1.latitude], "lon": [location.longitude, location1.longitude]}
+df = pd.DataFrame(data)
+st.map(df)
+
+# https://valhalla1.openstreetmap.de/route?json={%22costing%22:%22auto%22,%22exclude_polygons%22:[],%22locations%22:[{%22lon%22:-2.6819,%22lat%22:48.4882,%22type%22:%22break%22},{%22lon%22:2.3484,%22lat%22:48.8535,%22type%22:%22break%22}],%22directions_options%22:{%22units%22:%22kilometers%22},%22id%22:%22valhalla_directions%22}
